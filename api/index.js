86e32063265a7e8e3ce4e3001db763b14d75355e
Aditia Wiratama Putra <aditttginkgo@gmail.com>
2026-04-20 03:09:34 +0700

    update api/index.js

--- api/index.js
+++ api/index.js
@@ -2,9 +2,15 @@ export default function handler(req, res) {
     res.status(200).json({
         success: true,
         message: "Lumina REST API Serverless - Active",
-        version: "1.0.0",
+        version: "2.0.0",
         endpoints: {
-            tiktok: "/api/tiktok?url="
+            tiktok: "/api/tiktok?url=",
+            instagram: "/api/ig?url=",
+            youtube: "/api/yt?url=&format=audio/video",
+            soundcloud: "/api/soundcloud?url=",
+            spotify: "/api/spotify?url=",
+            pinterest: "/api/pinterest?url=",
+            twitter: "/api/twitter?url="
         }
     });
 }
